77600de00de264961e6eaa916d1d47c70c2aa823
Andrea Taroni <taroni.andrea1@gmail.com>
2023-04-14 14:14:03 +0200

    add generation of random point inside the sphere

--- include/spheremeshes/sphere.h
+++ include/spheremeshes/sphere.h
@@ -4,6 +4,9 @@
 #include <glm/geometric.hpp>
 #include <ostream>
 
+struct Point;
+class AABB;
+
 struct Sphere {
     Sphere();
     Sphere(glm::vec3 pCenter, float pRadius);
@@ -15,11 +18,12 @@ std::ostream& operator<<(std::ostream& ost, const Sphere& val);
 
 const Sphere& getBiggerSphere(const Sphere& s1, const Sphere& s2);
 
-//TODO
-// metodo che date due sfere restituisce la sfera più piccola che le contenga entrambe
 Sphere computeBoundingSphere(const Sphere& s1, const Sphere& s2);
 
 //TODO
 // metodo che restituisce punto casuale nella sfera
 // punto a caso nel cubo che contiene la sfera (3 numeri random x,y,z)
 // se è fuori dalla sfera, lo scarto e reitero finchè non è dentro.
+Point getRandomPointInSphere(const Sphere& sphere);
+
+AABB computeAABB(const Sphere& sphere);
--- src/spheremeshes/sphere.cpp
+++ src/spheremeshes/sphere.cpp
@@ -1,6 +1,9 @@
 #include <spheremeshes/sphere.h>
 #include <glm/common.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include <spheremeshes/point.h>
+#include <utils/random.h>
+#include <utils/aabb.h>
 
 Sphere::Sphere(glm::vec3 pCenter, float pRadius) : center(pCenter), radius(pRadius) {}
 
@@ -32,3 +35,26 @@ Sphere computeBoundingSphere(const Sphere& s1, const Sphere& s2)
     glm::vec3 C = s1.center + c2c1*(R - s1.radius) / c2c1mag;
     return Sphere(C, R);
 }
+
+Point getRandomPointInSphere(const Sphere& sphere) {
+    Point point;
+    const AABB& sphereAABB = computeAABB(sphere);
+    //infinite loop but it's okay, high probability of having a point inside the sphere in some iterations
+    while (true) {
+        glm::vec3 pointPos = generatePosition(sphereAABB);
+        glm::vec3 centerToPos = pointPos - sphere.center;
+        //generated position is inside the sphere, so return the Point with position pointPos
+        if (glm::dot(centerToPos, centerToPos) < sphere.radius*sphere.radius) {
+            return Point(pointPos, glm::vec3(0.0f));
+        }
+    }
+}
+
+AABB computeAABB(const Sphere& sphere) {
+    return AABB(
+        floatRange(sphere.center.x - sphere.radius, sphere.center.x + sphere.radius),
+        floatRange(sphere.center.y - sphere.radius, sphere.center.y + sphere.radius),
+        floatRange(sphere.center.z - sphere.radius, sphere.center.z + sphere.radius)
+    );
+}
+
