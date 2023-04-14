1cb557c55eac3891d9ebb2fbb104ff5838f6662b
Andrea Taroni <taroni.andrea1@gmail.com>
2023-04-14 09:47:06 +0200

    add point and coloredpoint constructors

---
+++ src/spheremeshes/point.cpp
@@ -0,0 +1,17 @@
+#include <spheremeshes/point.h>
+
+ Point::Point() : Point::Point(glm::vec3(0.0f), glm::vec3(0.0f)) {}
+
+ Point::Point(glm::vec3 pos, glm::vec3 normal) : pos(pos), normal(normal) {}
+
+ ColoredPoint::ColoredPoint() : Point(), color(glm::vec3(0.0f))
+ {
+ }
+
+ ColoredPoint::ColoredPoint(glm::vec3 pos, glm::vec3 normal, glm::vec3 color) : Point(pos, normal), color(color)
+ {
+ }
+
+ ColoredPoint::ColoredPoint(const Point &point, glm::vec3 color) : Point(point), color(color)
+ {
+ }
