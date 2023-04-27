1d2c19a680248bd6ab4434cd2652fc2f1c79c1a2
Andrea Taroni <taroni.andrea1@gmail.com>
2023-04-27 14:54:03 +0200

    add capsuloid struct comment

--- include/spheremeshes/capsuloid.h
+++ include/spheremeshes/capsuloid.h
@@ -3,6 +3,15 @@
 #include <glm/vec3.hpp>
 #include <utils/types.h>
 
+/* 
+    Struct that models a capsuloid (capsule with spheres of possibly different radius)
+    Spheres at the vertices are stored as uint that are indexes of the spheres vector in the sphere mesh
+
+    Factor, S0toS1 and sqrdL are constant features of the capsuloid that are chaced for performance:
+        - factor is float that indicates how to modify the parameter k, which is used to find the projection of a point on the capsuloid segment
+        - S0toS1 is the vector from sphere 0 to sphere 1
+        - sqrdL is the square magnitude of S0toS1
+*/
 struct Capsuloid {
     float factor;
     glm::vec3 S0toS1;
