817c38747512b8f940d4bb2d959c3f02e166900e
Andrea Taroni <taroni.andrea1@gmail.com>
2023-04-28 16:04:01 +0200

    add function to compute a dir from a position on the screen

--- include/utils/common.h
+++ include/utils/common.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 //check if n is in range [min, max] inclusive
 bool isInRangeIncl(float n, float min, float max);
 
 //check if n is in range (min, max) exclusive
 bool isInRangeExcl(float n, float min, float max);
+
+//compute the direction in world space from a position on the screen
+glm::vec3 screenToWorldDir(const glm::vec2& screenPos, float width, float height, const glm::mat4& viewMatrix, const glm::mat4 projectionMatrix);
--- src/utils/common.cpp
+++ src/utils/common.cpp
@@ -1,9 +1,24 @@
 #include <utils/common.h>
 
+using glm::vec3;
+using glm::vec4;
+
 bool isInRangeIncl(float n, float min, float max) {
     return n >= min && n <= max;
 }
 
 bool isInRangeExcl(float n, float min, float max) {
     return n > min && n < max;
+}
+
+glm::vec3 screenToWorldDir(const glm::vec2& screenPos, float width, float height, const glm::mat4& viewMatrix, const glm::mat4 projectionMatrix) {
+        float x = (2.0f * screenPos.x) / width - 1.0f;
+        float y = 1.0f - (2.0f * screenPos.y) / height;
+        float z = 1.0f;
+        vec3 ray_nds = vec3(x, y, z);
+        vec4 ray_clip = vec4(ray_nds.x, ray_nds.y, -1.0, 1.0);
+        vec4 ray_eye = glm::inverse(projectionMatrix) * ray_clip;
+        ray_eye = vec4(ray_eye.x, ray_eye.y, -1.0, 0.0);
+        vec3 ray_wor = glm::vec3((glm::inverse(viewMatrix) * ray_eye));
+        return glm::normalize(ray_wor);
 }
